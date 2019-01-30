a6bb623737fbb85dbcab297c8bc52c02b6f16772
Kamburugamuwa K.L.A.P.T.J <it17092548@my.sliit.lk>
2019-01-30 21:04:36 +0530

    add all

--- controller/adminControl.php
+++ controller/adminControl.php
@@ -2,18 +2,6 @@
 
 include_once('db.php');
 
-//$ella_id = $_POST["ella_id"];
-if( isset($_GET['submit']) ) {
-    //be sure to validate and clean your variables
-    $val1 = htmlentities($_GET['ella_id']);
-//    $val2 = htmlentities($_GET['val2']);
-
-    //then you can use them in a PHP function.
-//    $result = myFunction($val1, $val2);
-
-    echo $val1;
-    echo '<br/>';
-    $sql = "SELECT comment,name,email,website from comment_ella_srilanka where id = $val1";
 $ella_id = $galle_id = $udwalawe_id = $anuradapura_id = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
