ea0180dd894a3a0bf7a56efaa6a0ca36d14058a0
Kamburugamuwa K.L.A.P.T.J <it17092548@my.sliit.lk>
2019-01-31 08:39:33 +0530

    add all

--- controller/adminControl.php
+++ controller/adminControl.php
@@ -2,12 +2,12 @@
 
 include_once('db.php');
 
-<<<<<<< HEAD
+
 $ella_id = $galle_id = $udwalawe_id = $anuradapura_id = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-=======
+
 //$ella_id = $_POST["ella_id"];
 if( isset($_GET['submit']) ) {
     //be sure to validate and clean your variables
@@ -15,7 +15,6 @@ if( isset($_GET['submit']) ) {
     echo $val1;
     echo '<br/>';
     $sql = "SELECT comment,name,email,website from comment_ella_srilanka where id = $val1";
->>>>>>> ca515a4e14e33d17f05f2b9686fa30591299f0ca
 
 }
 
